e226172f9c1c5ca4dc9d2822e97ee407a12bb303
Marian Volytskyi <volmark00@gmail.com>
2023-10-12 14:49:21 +0300

    finish first lesson

---
+++ src/todos.js
@@ -0,0 +1,6 @@
+export default [
+  {id:1, title: 'HTML', completed:true},
+  {id:2, title: 'JS', completed:true},
+  {id:3, title: 'Vue', completed:false},
+  {id:3, title: 'TS', completed:false},
+]
